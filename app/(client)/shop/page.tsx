4631779e4f65d95ef04ba6155e6bdc1e9266d332
mayank kumar <162879224+mayankgit22@users.noreply.github.com>
2025-07-24 00:12:54 +0530

    Update page.tsx

--- app/(client)/shop/page.tsx
+++ app/(client)/shop/page.tsx
@@ -115,7 +115,7 @@ const filteredProducts = products.filter((product) => {
    if (
     category !== "All" &&
     !product.categories?.some((cat) =>
-      cat.toLowerCase().includes(category.toLowerCase())
+      cat._ref.toLowerCase().includes(category.toLowerCase())
     )
   ) {
     return false;
